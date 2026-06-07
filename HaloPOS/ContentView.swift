1b2108adcb2f55536bb7023a3915517f00920ab9
Putut <pututyb@gmail.com>
2026-06-07 20:12:51 +0700

    Initial Commit

---
+++ HaloPOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaloPOS
+//
+//  Created by Amelia Putri Oktaviani on 07/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaloPOS/HaloPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaloPOSApp.swift
+//  HaloPOS
+//
+//  Created by Amelia Putri Oktaviani on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HaloPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
